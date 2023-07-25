f65aab0bd1ebe85dc7ddb499489b987c68a04060
Chethan L <chethan.shantha@gmail.com>
2023-07-25 07:09:23 +0530

    changes in strops.py program

--- strops.py
+++ strops.py
@@ -1,32 +1,14 @@
 input_text = str(input("Enter the text: "))
 
+nupper = nlower = ndigits = 0
 
-def count_digits(text: str) -> int:
-    count = 0
-    for i in text:
-        if i.isdigit():
-            count += 1
-    return count
+for ch in input_text:
+    if ch.isdigit():
+        ndigits += 1
+    elif ch.islower():
+        nlower += 1
+    elif ch.isupper():
+        nupper += 1
 
-
-def count_uppercase(text: str) -> int:
-    count = 0
-    for i in text:
-        if i.isupper():
-            count += 1
-    return count
-
-
-def count_lowercase(text: str) -> int:
-    count = 0
-    for i in text:
-        if i.islower():
-            count += 1
-    return count
-
-
-print("The number of words in the text is: ", len(input_text.split()))
-print("The number of characters in the text is: ", len(input_text))
-print("The number of digits in the text is: ", count_digits(input_text))
-print("The number of uppercase letters in the text is: ", count_uppercase(input_text))
-print("The number of lowercase letters in the text is: ", count_lowercase(input_text))
+print(
+    f"No of :\nwords : {len(input_text.split())}\ncharacter : {len(input_text)}\nnumerical digits : {ndigits}\nuppercase char : {nupper}\nlowercase char : {nlower}")
